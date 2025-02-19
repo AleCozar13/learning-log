7e36264ed1c0a17da63f276c4c2fd85f132c7985
Ale Cózar <alejandrocozar@icloud.com>
2025-02-19 15:26:28 +0100

    Home Page URL

---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,12 @@
+"""Dependencies"""
+
+from django.urls import path
+from . import views
+
+# Defines URL patterns for learning_logs:
+app_name = "learning_logs"
+
+urlpatterns = [
+    # Home page
+    path("", views.index, name="index")
+]
